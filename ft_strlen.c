58168d209ef911817a62ea03c9ec81e1e1859c42
Alexis Montelimart <amonteli@z1r9p6.le-101.fr>
2019-10-08 14:32:19 +0200

    :hammer: ref: ft_strlen return size_t

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,12 +6,14 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 13:48:53 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 14:00:42 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/08 11:12:01 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int		ft_strlen(char *str)
+#include <stdio.h>
+
+size_t		ft_strlen(const char *str)
 {
 	int		count;
 
