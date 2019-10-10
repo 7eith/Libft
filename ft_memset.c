77b437fbc8e8d6cb4960b9c801cbc68f5e55d72e
Alexis Montelimart <amonteli@z1r9p1.le-101.fr>
2019-10-10 18:36:14 +0200

    :sparkles: add: memset

---
+++ ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_memset.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/09 18:43:50 by amonteli     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/10 18:36:03 by amonteli    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t		count;
+	unsigned char	*str;
+
+	count = 0;
+	str = s;
+	while (count < n)
+	{
+		str[count] = c;
+		count++;
+	}
+	return ((void *) str);
+}
