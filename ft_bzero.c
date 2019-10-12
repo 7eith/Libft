2da0519d2a83d21bb7a97ddc2145907b7f9ad02f
Alexis Montelimart <amonteli@z3r5p1.le-101.fr>
2019-10-12 13:56:19 +0200

    :sparkles: add: ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,14 +6,14 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 18:36:28 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 18:38:45 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/12 13:55:10 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	size_t			count;
 	unsigned char	*str;
