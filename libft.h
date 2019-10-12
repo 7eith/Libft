328b5ca17903a79b511fb5dd535860987885a972
Alexis Montelimart <amonteli@z3r5p1.le-101.fr>
2019-10-12 20:09:05 +0200

    :sparkles: impl: memccpy

---
+++ libft.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   libft.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/12 14:03:52 by amonteli     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/12 18:42:04 by amonteli    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *str, const char *search, size_t len);
+
+int		ft_atoi(const char *str);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+
+#endif
