955cadacd80f2e7b1a765d921bf03781734d2f36
Alexis Montelimart <amonteli@z1r9p1.le-101.fr>
2019-10-13 21:24:13 +0200

    :sparkles: impl. less code and add libft to include

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,23 +6,18 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 18:36:28 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/12 13:55:10 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/13 21:23:36 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	size_t			count;
-	unsigned char	*str;
 
-	count = 0;
-	str = s;
-	while (count < n)
-	{
-		str[count] = '\0';
-		count++;
-	}
+	count = -1;
+	while (++count < n)
+		((unsigned char *)s)[count] = '\0';
 }
--- ft_memset.c
+++ ft_memset.c
@@ -6,24 +6,19 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 18:43:50 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 18:37:39 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/13 21:23:39 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t			count;
-	unsigned char	*str;
 
-	count = 0;
-	str = s;
-	while (count < n)
-	{
-		str[count] = c;
-		count++;
-	}
-	return ((void *)str);
+	count = -1;
+	while (++count < n)
+		((unsigned char *)s)[count] = c;
+	return ((void *)s);
 }
