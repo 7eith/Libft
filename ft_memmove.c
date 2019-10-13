4ee9bc6fbfe0c3803492e89b824495e5268392d6
Alexis Montelimart <amonteli@z1r9p1.le-101.fr>
2019-10-13 20:46:54 +0200

    :hammer: ref: delete include

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,14 +6,12 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/12 20:32:40 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/13 18:39:35 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/13 18:44:31 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
-#include <stdio.h>
 
 void	*ft_memmove(void *dest, const void *source, size_t n)
 {
