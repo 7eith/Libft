bf3c518007e77af42c440be0421bf281a2a68229
Alexis Montelimart <amonteli@z1r9p1.le-101.fr>
2019-10-13 20:46:38 +0200

    :sparkles: add: memchr (abort on last test but seem ok)

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_memchr.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/13 18:46:14 by amonteli     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/13 19:56:17 by amonteli    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			count;
+	unsigned char	*str;
+
+	count = 0;
+	str = (unsigned char *)s;
+	while (count < n)
+	{
+		if (str[count] == (unsigned char)c)
+			return ((void *)str + count);
+		count++;
+	}
+	return (0);
+}
