2bf51cfbbc227e4cd8ae0a1c2548786c627ae61f
Alexis Montelimart <amonteli@z1r9p1.le-101.fr>
2019-10-13 20:47:17 +0200

    :hammer: ref: include libft, return without useless cast

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,12 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/12 13:56:35 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/12 15:32:46 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/13 16:19:26 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -23,11 +23,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	source = (char *)src;
 	d = (char *)dest;
 	if (n == 0)
-		return ((void *)d);
+		return (d);
 	while (count < n)
 	{
 		d[count] = source[count];
 		count++;
 	}
-	return ((void *)d);
+	return (d);
 }
