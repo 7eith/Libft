9f6acaf9908cd3a21df72f94f1d94a26b8e5a17d
Alexis Montelimart <amonteli@z3r6p2.le-101.fr>
2019-10-17 21:41:10 +0200

    :hammer: impr. minimize code

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/15 16:04:22 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/15 19:20:15 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/17 20:21:26 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,13 +17,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	count;
 	size_t	s1_size;
-	size_t	s2_size;
 	char	*tab;
 
 	count = -1;
 	s1_size = ft_strlen(s1);
-	s2_size = ft_strlen(s2);
-	if (!(tab = (char *)malloc((s1_size + s2_size + 1) * sizeof(char))))
+	if (!(tab = (char *)malloc((s1_size + ft_strlen(s2) + 1) * sizeof(char))))
 		return (NULL);
 	while (s1[++count])
 		tab[count] = s1[count];
