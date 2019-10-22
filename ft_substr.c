9645f62f7a54b98764aee62ca5a4be67368b98c0
Alexis Montelimart <amonteli@z2r1p2.le-101.fr>
2019-10-22 23:43:19 +0200

    :hammer: ref: protected to null s

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/15 14:13:51 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/15 15:56:06 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/22 23:42:37 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,6 +20,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	count = 0;
 	tab = 0;
+	if (!s)
+		return (0);
 	if (!(tab = (char *)malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	while (len > 0 && s[start + count] && ft_strlen(s) > start)
