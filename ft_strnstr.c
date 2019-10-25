c88f021394ddb7def90bf80c3038e00052262f48
Alexis Montelimart <amonteli@z3r4p1.le-101.fr>
2019-10-25 16:45:01 +0200

    :hammer: ref & add: ref norme, add bonus for norme, and rename params of strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,28 +6,28 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 18:44:30 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/25 15:48:30 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/25 16:43:29 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *s, const char *need, size_t len)
 {
 	size_t		i;
 	size_t		j;
 
 	i = 0;
 	j = 0;
-	if (little[0] == '\0')
-		return ((char *)big);
-	while (big[i] && i < len)
+	if (need[0] == '\0')
+		return ((char *)s);
+	while (s[i] && i < len)
 	{
-		while (big[i + j] == little[j] && little[j] && i + j < len)
+		while (s[i + j] == need[j] && need[j] && i + j < len)
 			j++;
-		if (little[j] == '\0')
-			return ((char *)&big[i]);
+		if (need[j] == '\0')
+			return ((char *)&s[i]);
 		i++;
 		j = 0;
 	}
