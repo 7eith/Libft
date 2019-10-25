ea68eab776dc161443358dd235c1e5de467d8ec7
Alexis Montelimart <amonteli@z3r4p1.le-101.fr>
2019-10-25 15:59:32 +0200

    :hammer: ref: prototype, seem good for all

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,26 +6,26 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/12 20:32:40 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/17 20:09:40 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/25 15:53:12 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *source, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*src;
+	char	*source;
 	char	*dst;
 
-	src = (char *)source;
+	source = (char *)src;
 	dst = dest;
-	if (!src && !dst)
+	if (!source && !dst)
 		return (NULL);
-	if (dst < src)
-		return (ft_memcpy(dst, src, n));
+	if (dst < source)
+		return (ft_memcpy(dst, source, n));
 	n++;
 	while (--n)
-		dst[n - 1] = src[n - 1];
+		dst[n - 1] = source[n - 1];
 	return (dst);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,29 +6,29 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/13 22:31:18 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/14 01:16:52 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/25 15:57:30 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t			ft_strlcat(char *dest, const char *src, size_t size)
+size_t			ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	count;
 	size_t	dest_size;
 	size_t	src_size;
 
 	count = 0;
-	dest_size = ft_strlen(dest);
+	dest_size = ft_strlen(dst);
 	src_size = ft_strlen(src);
 	if (dest_size >= size)
 		return (src_size + size);
 	while (src[count] && (size - 1 > count + dest_size))
 	{
-		dest[count + dest_size] = src[count];
+		dst[count + dest_size] = src[count];
 		count++;
 	}
-	dest[count + dest_size] = '\0';
+	dst[count + dest_size] = '\0';
 	return (dest_size + src_size);
 }
