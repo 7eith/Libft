136e43b224a7540f425069bc0ef7b414e4913c74
Alexis Montelimart <amonteli@z4r3p1.le-101.fr>
2019-10-31 17:01:07 +0100

    :hammer: fix: remove check for mem (mem not see '\0')

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/12 17:16:51 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 19:00:54 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/31 16:59:38 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -22,7 +22,9 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	count = 0;
 	source = (unsigned char *)src;
 	d = (unsigned char *)dest;
-	while (count < n && source[count])
+	if (!src && !dest)
+		return (NULL);
+	while (count < n)
 	{
 		d[count] = source[count];
 		if (source[count] == (unsigned char)c)
